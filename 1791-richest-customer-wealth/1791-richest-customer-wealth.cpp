54cf84cfcc971f62913e325068b7c679a9e2733e
sanjay10985 <95925904+sanjay10985@users.noreply.github.com>
2024-06-14 08:23:19 +0530

    Time: 3 ms (79.69%), Space: 10.1 MB (66.49%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.cpp
@@ -3,9 +3,8 @@ public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int maxwealth = 0;
         for(int i = 0;i<accounts.size();i++){
-            vector<int> person = accounts[i];
             int wealth = 0;
-            for(int j =0;j<person.size();j++){
+            for(int j =0;j<accounts[i].size();j++){
                 wealth+=accounts[i][j];
             }
             maxwealth = max(maxwealth,wealth);

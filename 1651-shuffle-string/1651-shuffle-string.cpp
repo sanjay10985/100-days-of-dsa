ebf6103d33e52d8bd0e730d69e7ec23350af19e3
sanjay10985 <95925904+sanjay10985@users.noreply.github.com>
2024-07-21 17:06:07 +0530

    Time: 4 ms (81.12%), Space: 18.4 MB (20.17%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string ans(s.length(),' ');
+        for(int i=0;i<s.length();i++ ){
+            ans[indices[i]]= s[i];
+
+        }
+        return ans;
+    }
+};

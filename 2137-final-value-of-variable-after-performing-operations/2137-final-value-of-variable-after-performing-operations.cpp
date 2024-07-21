83330c0b61d431ea5ff243889fdb10a58572d122
sanjay10985 <95925904+sanjay10985@users.noreply.github.com>
2024-07-21 16:39:44 +0530

    Time: 13 ms (6.24%), Space: 18.1 MB (10.36%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -2,11 +2,19 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int ans = 0;
-    for(int i = 0;i<operations.size();i++){
-        if(operations[i] == "++X" || operations[i] == "X++" ){
+    // for(int i = 0;i<operations.size();i++){
+    //     if(operations[i] == "++X" || operations[i] == "X++" ){
+    //         ans++;
+    //     }
+    //     if(operations[i] == "--X" || operations[i] == "X--" ){
+    //         ans--;
+    //     }
+    // }
+    for(string str: operations){
+        if(str[1] == '+'){
             ans++;
         }
-        if(operations[i] == "--X" || operations[i] == "X--" ){
+        else{
             ans--;
         }
     }

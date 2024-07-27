07c5ecfc3adf67c07d7e963ebb039027ff9a5edc
sanjay10985 <95925904+sanjay10985@users.noreply.github.com>
2024-07-27 22:13:34 +0530

    Time: 57 ms (18.86%), Space: 33.9 MB (29.07%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -4,16 +4,13 @@ public:
         unordered_set<char> compare(allowed.begin(),allowed.end());
         int ans= 0;
         for(string s: words){
-            int correct = false;
+            int correct = true;
             for(int i = 0;i<s.length();i++){
-                if(compare.find(s[i]) != compare.end()){
-                    correct = true;
-                    continue;
-                }
-                else{
+                if(compare.find(s[i]) == compare.end()){
                     correct = false;
                     break;
                 }
+              
             }
             if(correct){
                 ans++;

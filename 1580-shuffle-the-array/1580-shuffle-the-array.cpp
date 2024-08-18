e28d05f41dea1b50a09c1a9b35c360d04359f93c
sanjay10985 <95925904+sanjay10985@users.noreply.github.com>
2024-08-18 19:23:35 +0530

    Time: 4 ms (59.38%), Space: 12.4 MB (44.34%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    vector<int> shuffle(vector<int> &nums, int n)
-    {
-        vector<int> array(2 * n);
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2 * n);
         int j = 0;
-        for (int i = 0; i < 2 * n; i += 2)
-        {
-            array[i] = nums[j++];
-        }
-        j = n;
-        for (int i = 1; i < 2 * n; i += 2)
-        {
-            array[i] = nums[j++];
-        }
+        int k = n;
 
-        return array;
+        for(int i = 0;i<2*n;i+=2){
+            ans[i] = nums[j++];
+            ans[i+1] = nums[k++];
+        }
+        
+        return ans;
     }
-  
 };

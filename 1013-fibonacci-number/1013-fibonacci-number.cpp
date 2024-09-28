4a3c0c106685d982cfd3b60a6f133d5e8aca2cea
sanjay10985 <95925904+sanjay10985@users.noreply.github.com>
2024-09-28 22:30:52 +0530

    Time: 3 ms (66.48%), Space: 7.5 MB (34.33%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    int dp[31] = {0};
+
     int fib(int n) {
         if(n== 0 || n==1){
             return n;
         }
-        if(dp[n] != 0) return dp[n];
-        return dp[n]=fib(n-1)+fib(n-2);
+        vector<int> dp(n+1,-1);
+        dp[0]= 0;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };

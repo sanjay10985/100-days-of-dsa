4efd3a2ecb8ef2887c847ed2aec257a65d8e8b51
sanjay10985 <95925904+sanjay10985@users.noreply.github.com>
2024-09-28 16:33:22 +0530

    Time: 0 ms (100%), Space: 14.5 MB (81.83%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
@@ -3,12 +3,71 @@ public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
+        int left = 0;
+        int right = n-1;
         vector<int> ans;
-        for(int i = 0;i<n;i++){
-            if(nums[i] == target){
-                ans.push_back(i);
-            }
+
+       int first = findFirstOccurrence(nums,target);
+       int last = findLastOccurrence(nums,target);
+
+       if (first == -1 || last == -1) {
+        return {};
         }
+
+        for(int i = first; i<=last; i++){
+            ans.push_back(i);
+        }
+
+
         return ans;
     }
+
+    int findFirstOccurrence(vector<int>& nums, int target){
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int firstIndex = -1;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                firstIndex = mid;
+                high = mid-1;
+            }
+            else if(nums[mid] > target){
+                high = mid-1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return firstIndex;
+
+    }
+
+    int findLastOccurrence(vector<int>& nums, int target){
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int lastIndex = -1;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                lastIndex = mid;
+                low = mid+1;
+            }
+            else if(nums[mid] > target){
+                high = mid-1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return lastIndex;
+
+    }
+
 };

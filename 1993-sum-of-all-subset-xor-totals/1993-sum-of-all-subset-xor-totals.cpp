ee032692f07697fed9e17f1d4607818e527d3317
sanjay10985 <95925904+sanjay10985@users.noreply.github.com>
2024-10-05 14:47:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (58.08%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,44 +1,14 @@
 class Solution {
-private:
-
-void solve(vector<int> nums,int index, vector<int> output, vector<vector<int>>& ans){
-    if(index >= nums.size()){
-        ans.push_back(output);
-        return;
-    }
-
-    // exclude
-    solve(nums,index+1,output,ans);
-    // include
-    int element = nums[index];
-    output.push_back(element);
-    solve(nums,index+1,output,ans);
-}
-
-int xorOfArrayElements(vector<int> nums){
-    int n = nums.size();
-    int sum = 0;
-    for(int num:nums){
-        sum^=num;
-    }
-    return sum;
-}
 
 public:
     int subsetXORSum(vector<int>& nums) {
-        int n =nums.size();
-        vector<vector<int>> ans;
-        vector<int> output;
+          int xorSum = 0;
         
-        solve(nums,0,output,ans);
-
-        int xorSum = 0;
-
-        for(int i = 0;i<ans.size();i++){
-            int sum = xorOfArrayElements(ans[i]);
-            xorSum += sum;
+        // XOR all elements of the array
+        for (int num : nums) {
+            xorSum |= num;
         }
         
-        return xorSum;
+        return xorSum * (1 << (nums.size() - 1));
     }
 };
